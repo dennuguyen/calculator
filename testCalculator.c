740d1cb5a6d011b9912d0eb83f83c5a3ba845c4b
dan <dennuguyen@gmail.com>
2019-09-16 16:53:39 +1000

    Initial commit

---
+++ testCalculator.c
@@ -0,0 +1,91 @@
+// testCalculator.c
+// Dan Nguyen
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Stack.h"
+#include "Calculator.h"
+
+void calculatorTest(Stack s) {
+
+    additionTest(s);
+    subtractionTest(s);
+    multiplicationTest(s);
+    divisionTest(s);
+    powerTest(s);
+    factorialTest(s);
+}
+
+void additionTest(Stack s) {
+
+    // int + int
+
+
+    // float + float
+
+    // float + int
+
+    // int + float
+
+    // ++
+}
+
+void subtractionTest(Stack s) {
+    
+    // int - int
+
+    // float - float
+
+    // - float - int
+
+    // int - float
+
+    // --
+
+    // +-
+
+    // -+
+
+    // ---
+
+    // ----
+}
+
+void multiplicationTest(Stack s) {
+    
+    // int * int
+
+    // float * float
+
+    // int * float
+
+    // float * int
+
+    // **
+
+    // int * -int
+
+    // -int * int
+
+    // float * - float
+
+    // -float * float
+}
+
+void divisionTest(Stack s) {
+
+
+}
+
+void powerTest(Stack s) {
+
+
+}
+
+void factorialTest(Stack s) {
+
+
+}
