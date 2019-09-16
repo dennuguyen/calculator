90fde9ef860ec0808164a87720173f39f217b81e
dan <dennuguyen@gmail.com>
2019-09-16 20:18:25 +1000

    Add test cases

--- testCalculator.c
+++ testCalculator.c
@@ -9,26 +9,55 @@
 #include "Stack.h"
 #include "Calculator.h"
 
-void calculatorTest(Stack s) {
+void testLoop(Stack s, char* infix) {
+    reset(s);
+    convert(s, infix);
+    evaluate(s);
+    output(s);
+}
 
+void checkAnswer(Stack s, double answer) {
+    if (s->valStack[s->valTop] == answer) {
+        printf("Passed!\n\n");
+    } else {
+        printf("Failed! Answer = %f\n", answer);
+    }
+}
+
+void testCalculator(Stack s) {
+    
     additionTest(s);
-    subtractionTest(s);
+    /*subtractionTest(s);
     multiplicationTest(s);
     divisionTest(s);
     powerTest(s);
-    factorialTest(s);
+    factorialTest(s);*/
 }
 
 void additionTest(Stack s) {
 
-    // int + int
+    char infix[MAX_INPUT];
+    char answer[MAX_INPUT];
 
+    // int + int
+    strcpy(infix, "1941 + 319\n");
+    testLoop(s, infix);
+    checkAnswer(s, 2260);
 
     // float + float
+    strcpy(infix, "1931.1391041 + 4294.191000\n");
+    testLoop(s, infix);
+    checkAnswer(s, 6225.330104);
 
     // float + int
+    strcpy(infix, "1931.1391041 + 4294\n");
+    testLoop(s, infix);
+    checkAnswer(s, 6225.139104);
 
     // int + float
+    strcpy(infix, "4911 + 648.194108\n");
+    testLoop(s, infix);
+    checkAnswer(s, 5559.194108);
 
     // ++
 }
